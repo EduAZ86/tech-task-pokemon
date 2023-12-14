14c2efb8e687f4d8eb2e9ab5fed63d190bc3ab5e
EduAZ86 <eduardoayaviri@gmail.com>
2023-12-14 09:32:57 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tech-task-pokemon-ionic-react-gql',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
